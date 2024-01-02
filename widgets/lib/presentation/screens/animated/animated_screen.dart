c00495c4248a22c6f2cfa775b5d993b779f9cb07
Isa-josep <isaurini1902@gmail.com>
2024-01-02 14:34:02 -0600

    radius top-start-end-buttomStart-buttomEnd

--- widgets/lib/presentation/screens/animated/animated_screen.dart
+++ widgets/lib/presentation/screens/animated/animated_screen.dart
@@ -14,13 +14,24 @@ class _AnimatedScreenState extends State<AnimatedScreen> {
   double height = 100;
   Color color = Colors.indigo;
   double radius = 10.0;
-
+  double topStart = 10.0;
+  double topEnd = 10.0;
+  double bottomStart = 10.0;
+  double bottomEnd = 10.0;
   void cambiarForma(){
     final random = Random();
 
     width = random.nextInt(320)+40;
     height = random.nextInt(320)+40; // se agrega el 50, ya que puede generar 0 y si es ese caso se suman 50
     radius = random.nextDouble()*100;
+    topStart = random.nextDouble()*100;
+    topEnd = random.nextDouble()*100;
+    bottomStart = random.nextDouble()*100;
+    bottomEnd = random.nextDouble()*100;
+    print(topStart);
+    print(topEnd);
+    print(bottomStart);
+    print(bottomEnd);
     color = Color.fromRGBO(
       random.nextInt(255),
       random.nextInt(255),
@@ -47,7 +58,13 @@ class _AnimatedScreenState extends State<AnimatedScreen> {
           height: height <= 0 ? 0:height, //evitar que el height sea 0
           decoration: BoxDecoration(
             color: color,
-            borderRadius: BorderRadius.circular(radius<0 ? 0:radius), 
+
+            borderRadius: BorderRadiusDirectional.only(
+              topStart: Radius.circular(topStart),
+              topEnd: Radius.circular(topEnd),
+              bottomStart: Radius.circular(bottomStart),
+              bottomEnd: Radius.circular(bottomEnd),
+            ) 
           ),
         ),
       ),
