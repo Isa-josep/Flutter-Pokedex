5037012400f13ae8e5e90d622188b76c0e0d741d
Isa-josep <isaurini1902@gmail.com>
2024-02-12 22:17:52 -0600

    Firts commit

---
+++ push_notifications/lib/presentation/home_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('permisos'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 0,
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile();
+      },
+    );
+  }
+}
